0089aad24be8c471adccc2cc443ad4d01763092e
Stephan Köninger <mail@stephan-koeninger.de>
2013-04-19 23:00:58 +0200

    Wicket:child wicket:extend

---
+++ wicket.quickstart/src/main/java/de/stekoe/c4/wicket/quickstart/BasePage.java
@@ -0,0 +1,13 @@
+package de.stekoe.c4.wicket.quickstart;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class BasePage extends WebPage {
+	private static final long serialVersionUID = 4703212991485587319L;
+
+	public BasePage(final PageParameters pageParameters) {
+		super(pageParameters);
+	}
+	
+}
