c76e0c8dbee4756242627758ef0c4d7371da1acd
Stephan Köninger <mail@stephan-koeninger.de>
2013-04-24 13:41:50 +0200

    ein bild

--- wicket.quickstart/src/main/java/de/stekoe/c4/wicket/quickstart/HomePage.java
+++ wicket.quickstart/src/main/java/de/stekoe/c4/wicket/quickstart/HomePage.java
@@ -1,7 +1,10 @@
 package de.stekoe.c4.wicket.quickstart;
 
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.request.resource.ContextRelativeResource;
+import org.apache.wicket.request.resource.ResourceReference;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.image.Image;
 import org.apache.wicket.markup.html.WebPage;
 
 public class HomePage extends BasePage {
@@ -9,5 +12,7 @@ public class HomePage extends BasePage {
 
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
+		
+		add(new Image("exampleImage", new ContextRelativeResource("/img/logo.png")));
     }
 }
